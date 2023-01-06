5bd1637ba1fd53fe5bce2086f7c0ccdb464cf022
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-06 15:05:32 +0700

    Adding initial pages :sparkles:

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,12 @@ const App = () => {
   return (
     <div className="relative sm:p-8 p-4 bg-[#131318] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 relative">Sidebar</div>
+
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         Navbar
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </div>
   );
