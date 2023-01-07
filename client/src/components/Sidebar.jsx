0053f3e92ba5c34008229d1f8c3cb7ede511645c
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-07 14:32:03 +0700

    Adding hooks and style on container :sparkles:

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,7 +5,16 @@ import { logo, sun } from '../assets';
 import { navlinks } from '../constants';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState('dashboard');
+
+  return (
+    <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+      <Link to="/">
+        <Icon />
+      </Link>
+    </div>
+  );
 };
 
 export default Sidebar;
