36a9e84ede1d686f5fd4ea3acf1188e07a24e5ff
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-07 15:04:45 +0700

    Editing Icon component inside Sidebar component :hammer:

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -8,9 +8,21 @@ const Sidebar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState('dashboard');
 
-  const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => {
-    <div className={`w-[48px] h-[48px] rounded-[10px]`}></div>;
-  };
+  const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
+    <div
+      className={`w-[48px] h-[48px] rounded-[10px] ${
+        isActive && isActive === name && 'bg-[#2c2f32]'
+      } flex justify-center items-center ${
+        !disabled && 'cursor-pointer'
+      } ${styles}`}
+    >
+      {!isActive ? (
+        <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+      ) : (
+        <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+      )}
+    </div>
+  );
 
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
