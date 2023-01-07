1b0a160907a92cd0d3949fa3b364887d2370b442
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-08 05:58:17 +0700

    Editing Sidebar component :hammer:

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -15,11 +15,16 @@ const Sidebar = () => {
       } flex justify-center items-center ${
         !disabled && 'cursor-pointer'
       } ${styles}`}
+      onClick={handleClick}
     >
       {!isActive ? (
         <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
       ) : (
-        <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+        <img
+          src={imgUrl}
+          alt="fund_logo"
+          className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`}
+        />
       )}
     </div>
   );
@@ -29,6 +34,24 @@ const Sidebar = () => {
       <Link to="/">
         <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
       </Link>
+
+      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
+        <div className="flex flex-col justify-center items-center gap-3">
+          {navlinks.map((link) => (
+            <Icon
+              key={link.name}
+              {...link}
+              isActive={isActive}
+              handleClick={() => {
+                if (!link.disabled) {
+                  setIsActive(link.name);
+                  navigate(link.link);
+                }
+              }}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
