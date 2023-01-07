7ef1e2b22fcd5fecf6a90602f07a12d9dc81527f
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-08 06:08:14 +0700

    Adding initial container to Navbar component :sparkles:

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,23 @@ import { logo, menu, search, thirdweb } from '../assets';
 import { navlinks } from '../constants';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState('dashboard');
+  const [toggleDrawer, settoggleDrawer] = useState(false);
+
+  return (
+    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24]">
+        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
+          <img
+            src={search}
+            alt="search"
+            className="w-[15px] h-[15px] object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
