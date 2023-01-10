3a513b130f3176b6105a42ecbd09d77161ca1ea2
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-11 04:28:16 +0700

    Adding StateContextProvider inside contex index.js :sparkles:

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -8,3 +8,24 @@ import {
 import { ethers } from 'ethers';
 
 const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    '0x4edaeFAa06c747e01a3886aa82763E78E92ADDb3'
+  );
+
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    'createCampaign'
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    const data = await createCampaign([
+      address, //owner
+      form.title, //title
+    ]);
+  };
+};
