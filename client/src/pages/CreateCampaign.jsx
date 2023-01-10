027c0632a27a863a590a3e655f76322eb45fb8ca
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-11 03:59:54 +0700

    Adding context file and importing file :sparkles:

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -19,10 +19,14 @@ const CreateCampaign = () => {
   });
 
   const handleFormFieldChange = (fieldName, e) => {
-    setForm({ ...form, [fieldName]: e.target.val });
+    setForm({ ...form, [fieldName]: e.target.value });
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(form);
+  };
 
   return (
     <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
