73ae3178a618769b831b63bef499af32a080d98c
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-12 07:31:33 +0700

    Adding getCampaigns function in index of context

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -39,9 +39,21 @@ export const StateContextProvider = ({ children }) => {
     }
   };
 
+  const getCampaigns = async () => {
+    const campaigns = await contract.call('getCampaigns');
+
+    console.log(campaigns);
+  };
+
   return (
     <StateContext.Provider
-      value={{ address, contract, connect, createCampaign: publishCampaign }}
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+      }}
     >
       {children}
     </StateContext.Provider>
