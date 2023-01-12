80dcab9f07a3176bc6423998d0a4c970c0d421ed
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-12 14:47:07 +0700

    Adding useEffect and fetchCampaigns on Home page :sparkles:

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,6 +8,18 @@ const Home = () => {
 
   const { address, contract, getCampaigns } = useStateContext();
 
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getCampaigns();
+
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
   return <div>Home</div>;
 };
 
