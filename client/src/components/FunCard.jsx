cc7fd06cf6898ddd7f6cc76d5c256c01e07940ac
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-13 13:48:53 +0700

    Adding initial importing files to FunCard component :sparkles:

--- client/src/components/FunCard.jsx
+++ client/src/components/FunCard.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
-const FunCard = () => {
-  return <div>FunCard</div>;
+import { tagType, thirdweb } from '../assets';
+import { daysLeft } from '../utils';
+
+const FunCard = ({
+  owner,
+  title,
+  description,
+  target,
+  deadline,
+  amountCollected,
+  image,
+  handleClick,
+}) => {
+  const remainingDays = daysLeft(deadline);
+
+  return (
+    <div
+      className="sm:w-[288px] w-ful rounded-[15px] bg-[#1c1c24] cursor-pointer"
+      onClick={handleClick}
+    >
+      <img
+        src={image}
+        alt="fund"
+        className="w-full h-[158px] object-cover rounded-[15px]"
+      />
+    </div>
+  );
 };
 
 export default FunCard;
