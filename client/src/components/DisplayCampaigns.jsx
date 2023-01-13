88f5c9f6b48613117e844ac26c7dbb9e6ed53207
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-13 13:22:29 +0700

    Adding initial importing files to DisplayCampaigns component :sparkles:

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const DisplayCampaigns = () => {
-  return <div>DisplayCampaigns</div>;
+import { loader } from '../assets';
+
+const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">
+        {title}
+      </h1>
+    </div>
+  );
 };
 
 export default DisplayCampaigns;
