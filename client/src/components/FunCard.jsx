27622f49d232b8ea595380d8d10e77ed5f22fcd4
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-16 14:26:58 +0700

    Adding raised and days left description :sparkles:

--- client/src/components/FunCard.jsx
+++ client/src/components/FunCard.jsx
@@ -46,6 +46,25 @@ const FunCard = ({
             {description}
           </p>
         </div>
+
+        <div className="flex justify-between flex-wrap mt-[15px] gap-2">
+          <div className="flex flex-col">
+            <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">
+              {amountCollected}
+            </h4>
+            <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">
+              Raised of {target}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">
+              {remainingDays}
+            </h4>
+            <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">
+              Days Left
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
