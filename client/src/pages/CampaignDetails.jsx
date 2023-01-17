8eef8b3a517f9180f1a8d3d3d585f55545e60340
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-17 09:54:15 +0700

    Adding importing files and constant element :sparkles:

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { ethers } from 'ethers';
+
+import { useStateContext } from '../context';
+import { CustomButton } from '../components';
+import { calculateBarPercentage, daysLeft } from '../utils';
+import { thirdweb } from '../assets';
 
 const CampaignDetails = () => {
+  const { state } = useLocation();
+  const { getDonations, contract, address } = useStateContext();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [amount, setAmount] = useState('');
+  const [donators, setDonators] = useState([]);
+
+  const remainingDays = daysLeft(state.deadline);
+
   return <div>CampaignDetails</div>;
 };
 
