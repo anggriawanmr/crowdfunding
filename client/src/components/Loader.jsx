7dcac9b4a6b775d5b0eb1c5b2a7d4d81057c0065
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-20 00:32:03 +0700

    Adding initial importing files and container :sparkles:

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { loader } from '../assets';
+
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="fixed inset-0 z-10 h-screen bg-[rgba(0,0,0,0.7)] flex items-center justify-center flex-col">
+      Loader
+    </div>
+  );
 };
 
 export default Loader;
